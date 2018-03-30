c37c937f21fb96d6ed1ca529cfd3b4ad4d82acc8
Amrit Das <31342979+amrit-das@users.noreply.github.com>
2018-03-30 21:23:50 +0530

    imshow error fixed

--- src/imagePub.py
+++ src/imagePub.py
@@ -3,18 +3,18 @@ import cv2
 import numpy as np
 import time
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import Bool
 
 try:
 	index = sys.argv[1]
 except:
-	index = 0
+	index = 0 
 
 
 
 cap = cv2.VideoCapture(index)
 
-y,u,v = 0,124,68
+y,u,v = 0,62,215
 kernel = np.ones((5,5),np.uint8)
 
 width = cap.get(3)
@@ -28,30 +28,27 @@ def detect():
 	global cap,y,u,v,kernel,width,height,flag,area
 	
 	while flag:
-		#get_image
+		
 		ret,frame = cap.read()
 
-		#colour space conversion and masking
 		img_yuv = cv2.cvtColor(frame,cv2.COLOR_BGR2YUV)
 		mask = cv2.inRange(img_yuv, (np.array([0,u-25,v-25])), (np.array([255,u+25,v+25])))
 
-		#morphological_transformation_open
+		
 		erode = cv2.erode(mask,kernel,iterations = 1)
 		dilate = cv2.dilate(erode,kernel,iterations = 1)
 
-		#morphological_transformation_close
+		
 		erode = cv2.erode(mask,kernel,iterations = 1)
 		dilate = cv2.dilate(erode,kernel,iterations = 1)	
 
-		#contour detection
 		image,contour,hierarchy = cv2.findContours(dilate,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-		#cv2.drawContours(frame, contour, -1, (0,255,0), 2)
 		
-		#waitKey init
-		if cv2.waitKey(5) == 27:
-			flag = 0 
+		if cv2.waitKey(1) == 27:
+			flag = 0
+		 
 
-		#contour operations
+		
 		if contour:
 			#cnt = contour[0]
 			cnt = max(contour, key = cv2.contourArea)		#getting contour with max area
@@ -60,34 +57,35 @@ def detect():
 			cv2.circle(frame,(x,y),radius,(0,255,0),2)		#drawing circle across contour
 
 			area = radius*radius*3.14
-          
-			if area>1000:
-				return "detected"
+			cv2.imshow("Masking",mask)
+			cv2.imshow("YUV",frame)
+          		#return True
+			if area:
+				#print "detected"
 
+				return True
+			'''
 			else:
 				return "not in range"
 			
-
-			#print "area: ",area," ","x: ",x," ","y: ",y	
-
+		
+			print "area: ",area," ","x: ",x," ","y: ",y	
+			'''
 
 		else:
-
-			return "not"
-			#print "Contour Nahi He"
-
-					
-		cv2.imshow("Masking",mask)
-		cv2.imshow("YUV",frame)
+			#print "Not detected"
+			#print "Contour Nahi He				
+			cv2.imshow("Masking",mask)
+			cv2.imshow("YUV",frame)
+			return False
 		
 
-	cap.release()
-	cv2.destroyAllWindows()
+	
 
 
 def talker() :
 
-	pub=rospy.Publisher('detect',String,queue_size=10)
+	pub=rospy.Publisher('detect',Bool,queue_size=10)
 	rospy.init_node('talker',anonymous=True) 
 	rate=rospy.Rate(10)
 
@@ -100,8 +98,8 @@ if __name__=="__main__" :
 	try :
 		talker()
 	except rospy.ROSInterruptException :
-		pass
-
+		cap.release()
+		cv2.destroyAllWindows()
 
 
 
